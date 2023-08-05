4130eda67dbfffd75799b6719be72fd75c2c66e2
marko.v <markovukotic32@gmail.com>
2023-08-05 10:43:54 +0200

    Added business_role column in the database of the user, as well as bookie registration and also a boat owner aka. company registration

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -16,6 +16,15 @@
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
+        <div class="flex items-center mt-4">
+            <input checked id="bookie" type="radio" value='bookie' name="business_role" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+            <label for="bookie" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Bookie</label>
+        </div>
+        <div class="flex items-center mt-2">
+            <input id="company" type="radio" value='company' name="business_role" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+            <label for="company" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Company</label>
+        </div>
+
         <!-- Password -->
         <div class="mt-4">
             <x-input-label for="password" :value="__('Password')" />
